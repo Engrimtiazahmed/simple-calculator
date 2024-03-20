569202abc8d49a3547b8aafbfee4cbf0e4a35a9a
unknown <engineerimtiaz@yahoo.com>
2024-03-20 13:27:55 +0500

    pushing project of simple calculator

---
+++ index.js
@@ -0,0 +1,32 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { massage: "Please enter first number", type: "number", name: "firstNumber" },
+    {
+        massage: "Please enter second number",
+        type: "number",
+        name: "secondNumber",
+    },
+    {
+        massage: "Please sellect one of the operator to perform action",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Substraction", "Multiplication", "Division"],
+    },
+]);
+// Conditional statement 
+if (answer.operator === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "Substraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    console.log("Please enter a valid number");
+}
